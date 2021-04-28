86c3edf15dfaf6205b0d70f9edc09bd0b2bbbb70
Artur Sousa <artursousa505@gmail.com>
2021-04-28 12:14:01 -0300

    Project structure changed to class-based and created Home and Details

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  constructor(props: {}){
+    super(props);
+    this.state = {}
+    
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#add8e6',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/Details/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Details extends Component{
+    constructor(props: {}){
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+          <View>
+              <Text> Details Page </Text>
+          </View>  
+        );
+    }
+}
