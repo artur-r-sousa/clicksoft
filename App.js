8d74b225347839f7d22ccda7f4c47aee04b3a68b
Artur Sousa <artursousa505@gmail.com>
2021-04-28 12:51:04 -0300

    stacked navigation created for switching screens

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet } from 'react-native';
+import Home from './screens/Home';
+import Details from './screens/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +14,18 @@ export default class App extends Component {
     this.state = {}
     
   }
+
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName={Home}>
+          <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
+          <Stack.Screen name="Details" component={Details} options={{headerShown: false}}/>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
 }
 
 const styles = StyleSheet.create({
